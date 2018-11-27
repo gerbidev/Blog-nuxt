f692c8b9b3155df1e2dcfc0ff5226f4987848ba0
Gerbi <gerbi@protonmail.com>
2018-11-26 17:52:40 -0700

    create form using element ui

--- pages/post/_id/edit.vue
+++ pages/post/_id/edit.vue
@@ -1,10 +1,29 @@
 <template>
-    <h1>Edit Post</h1>
+  <div class="container">
+    <el-main>
+      <el-form ref="form" :model="form" label-width="120px">
+        <el-form-item label="Title">
+          <el-input v-model="form.title"></el-input>
+        </el-form-item>
+        <el-form-item label="Description">
+          <el-input v-model="form.description"></el-input>
+        </el-form-item>
+      </el-form>
+    </el-main>
+  </div>
 </template>
 
 <script>
   export default {
-    name: 'editPost'
+    name: 'editPost',
+    data(){
+      return {
+        form: {
+          title: '',
+          description: ''
+        }
+      }
+    }
   }
 </script>
 
